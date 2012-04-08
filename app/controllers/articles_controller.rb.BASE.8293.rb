dcb21829b4d3cf93891db63b6807a19e63d107b8
Michael Tighe <striderIIDX@gmail.com>
2012-04-08 03:06:10 -0500

    remove backup merge files

--- app/controllers/articles_controller.rb.BASE.8293.rb
+++
@@ -1,5 +0,0 @@
-class ArticlesController < ApplicationController
-  def index
-    @articles = Article.last(5)
-  end
-end
--- app/controllers/articles_controller.rb.LOCAL.8293.rb
+++
@@ -1,9 +0,0 @@
-class ArticlesController < ApplicationController
-  def index
-    @articles = Article.last(5)
-  end
-
-  def show
-    @article = Article.find(params[:id])
-  end
-end

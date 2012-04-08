a088b68a3f4e53378131e3e2bd6f43b78ba36e59
Michael Tighe <striderIIDX@gmail.com>
2012-04-08 03:32:21 -0500

    Destroy assicated tags w/ articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
   attr_accessible :title, :content, :user_id
+  after_destroy :cleanup
   
   belongs_to :user
   has_many :tags
@@ -11,4 +12,11 @@ class Article < ActiveRecord::Base
   def modified?
     created_at != updated_at
   end
+
+  private
+    def cleanup
+      if self.destroyed?
+        self.tags.destroy_all
+      end
+    end
 end

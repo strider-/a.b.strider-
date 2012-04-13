178bbd4245d17ca024006b91af56a9724549e5f8
Michael Tighe <striderIIDX@gmail.com>
2012-04-13 13:20:33 -0500

    Update article content to text

---
+++ db/migrate/20120413181852_update_article_content_to_text.rb
@@ -0,0 +1,5 @@
+class UpdateArticleContentToText < ActiveRecord::Migration
+  def change
+    change_column :articles, :content, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120408184328) do
+ActiveRecord::Schema.define(:version => 20120413181852) do
 
   create_table "articles", :force => true do |t|
     t.integer  "user_id"
     t.string   "title"
-    t.string   "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.text     "content",    :limit => 255
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
   end
 
   create_table "personal_links", :force => true do |t|
